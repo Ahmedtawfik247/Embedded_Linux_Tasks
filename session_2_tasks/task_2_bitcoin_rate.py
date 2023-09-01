bd42183d2de7f16683f037616e8fec7f0be15e58
Ahmedtawfik247 <ahmedbotsha2001@gmail.com>
2023-09-01 19:18:40 +0300

    modify the repo

--- session_2_tasks/task_2_bitcoin_rate.py
+++
@@ -1,7 +0,0 @@
-#----------------------------------------
-#code to find bitcoin rate automatically.
-#----------------------------------------
-import requests
-
-response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
-print(response.json()['bpi']['USD'])
--- session_3_tasks/my_location.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-url = requests.get('https://api.ipify.org/?format=json')
-url = url.json()['ip']
-loc = requests.get('https://ipinfo.io/'+url+'/geo')
-print(loc.json())
